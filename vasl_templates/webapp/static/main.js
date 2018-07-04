0f5c0be51f14154f23dc96f283efb97ab84c91bb
Taka <taka@vm-linux-dev>
2018-07-04 07:52:12 +0000

    Added the HTML framework for showing the content.

--- vasl_templates/webapp/static/main.js
+++ vasl_templates/webapp/static/main.js
@@ -2,18 +2,27 @@
 // --------------------------------------------------------------------
 
 $(document).ready( function () {
-    $("form").submit( function( evt ) {
-        evt.preventDefault() ;
-        var data = { val: $("form input[name='val']").val() } ;
-        $.post( {
-            url: gGenerateURL,
-            data: data,
-            success: function( data ) {
-                $("#response").text( data ) ;
-            },
-            error: function( xhr, status, errorMsg ) {
-                $("#response").text( "ERROR: "+errorMsg ) ;
-            },
+
+    // initialize
+    $("#tabs").tabs( {
+        heightStyle: "fill",
+    } ) ;
+    var navHeight = $("#tabs .ui-tabs-nav").height() ;
+
+    // FUDGE! CSS grids don't seem to update their layout vertically when
+    // inside a jQuery tab control - we do it manually :-/
+    var prevHeight = [] ;
+    $(window).resize( function() {
+        $(".ui-tabs-panel").each( function() {
+            $(this).css( "padding", "5px" ) ; // FUDGE! doesn't work when set in the CSS :-/
+            var id = $(this).attr( "id" ) ;
+            var h = $(this).parent().innerHeight() - navHeight - 20 ;
+            if ( h !== prevHeight[id] )
+            {
+                $(this).css( "height", h+"px" ) ;
+                prevHeight[id] = h ;
+            }
         } ) ;
     } ) ;
+    $(window).trigger( "resize" ) ;
 } ) ;
--- vasl_templates/webapp/tests/test_generate.py
+++ vasl_templates/webapp/tests/test_generate.py
@@ -1,37 +1,3 @@
 """ Test response generation. """
 
-from vasl_templates.webapp.tests.utils import find_child
-
 # ---------------------------------------------------------------------
-
-def test_generate( webapp, webdriver ):
-    """Test response generation."""
-
-    # initialize
-    webdriver.get( webapp.url_for( "main" ) )
-
-    # try saying something
-    textbox = find_child( webdriver, "input[type='text']" )
-    textbox.clear()
-    textbox.send_keys( "Hi, there!" )
-    submit = find_child( webdriver, "input[type='submit']" )
-    submit.click()
-    response = find_child( webdriver, "#response" )
-    assert response.text == 'You said: "Hi, there!"'
-
-    # try saying something else
-    textbox = find_child( webdriver, "input[type='text']" )
-    textbox.clear()
-    textbox.send_keys( "Yo mama so big..." )
-    submit = find_child( webdriver, "input[type='submit']" )
-    submit.click()
-    response = find_child( webdriver, "#response" )
-    assert response.text == 'You said: "Yo mama so big..."'
-
-    # try saying nothing
-    textbox = find_child( webdriver, "input[type='text']" )
-    textbox.clear()
-    submit = find_child( webdriver, "input[type='submit']" )
-    submit.click()
-    response = find_child( webdriver, "#response" )
-    assert response.text == "You said: nothing!"
