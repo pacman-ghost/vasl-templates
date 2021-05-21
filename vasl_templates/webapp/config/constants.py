1d3af963dfab67bb9ae6e97e9ec3cc74294bd0bb
Taka <taka@vm-linux-dev>
2021-05-21 20:41:59 +1000

    Updated the version strings.

--- vasl_templates/webapp/config/constants.py
+++ vasl_templates/webapp/config/constants.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 APP_NAME = "VASL Templates"
-APP_VERSION = "v1.6" # nb: also update setup.py
+APP_VERSION = "v1.7" # nb: also update setup.py
 APP_DESCRIPTION = "Generate HTML for use in VASL scenarios."
 APP_HOME_URL = "https://vasl-templates.org"
 
